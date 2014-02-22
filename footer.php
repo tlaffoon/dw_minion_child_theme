74cd9c94e3c9be025df73f8eae7bf69fc184ed86
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-02-22 07:22:08 -0600

    Add Custom Theme

---
+++ footer.php
@@ -0,0 +1,17 @@
+        </div>
+      </div>
+    </div>
+    <footer id="colophon" class="site-footer" role="contentinfo">
+      <div class="container">
+        <div class="site-info">
+			<?php include("./wp-includes/fortune.php"); ?>
+        </div>
+      </div>
+    </footer>
+  </div>
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ fortune.php
@@ -0,0 +1,1 @@
+<?php $output = shell_exec('/usr/bin/fortune'); echo "<pre>$output</pre>";?>
