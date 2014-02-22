ba78f4c21c6d36f2469b2d1f5ddb8be8d09b0628
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-02-22 07:48:17 -0600

    Fortune Enabled

--- footer.php
+++ footer.php
@@ -4,7 +4,11 @@
     <footer id="colophon" class="site-footer" role="contentinfo">
       <div class="container">
         <div class="site-info">
+
+			
 			<?php include("./wp-includes/fortune.php"); ?>
+
+
         </div>
       </div>
     </footer>
