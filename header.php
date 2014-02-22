e42cbab38c2f398a7eaf4a7292340583a37cdc08
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-02-22 07:54:20 -0600

    Disable fortune

--- header.php
+++ header.php
@@ -18,10 +18,6 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 
-
-	<?php include("./wp-includes/fortune.php"); ?>
-
-
 	<div class="container clearfix">
 		<?php do_action( 'before' ); ?>
 		<div id="navigation" class="site-nav">
